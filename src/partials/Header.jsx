df8597b415df26896b3df5ae42ea8110776e3ff5
Neo87z <shehanhoradagoda12@gmail.com>
2022-09-24 17:21:41 -0230

    'main'

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from '../utils/Dropdown';
-
+import MainEvent from '../partials/Match';
 import logo from '../images/logo1.png';
 function Header() {
 
@@ -39,11 +39,11 @@ function Header() {
           {/* Site branding */}
           <div className="shrink-0 mr-4">
             {/* Logo */}
-          
+
             <Link to="/" className="block" aria-label="Cruip">
-             
-              <img  src={logo} width="76" height="187" alt="Tabs 01" />
-           
+
+              <img src={logo} width="76" height="187" alt="Tabs 01" />
+
             </Link>
           </div>
 
@@ -51,12 +51,12 @@ function Header() {
           <nav className="hidden md:flex md:grow">
 
             {/* Desktop menu links */}
-          
+
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-       
+
               <li>
-               { /*<Link to="/signup" className="btn-sm text-white bg-green-600 hover:bg-green-700 ml-3">Telegram Login</Link>*/}
+                { /*<Link to="/signup" className="btn-sm text-white bg-green-600 hover:bg-green-700 ml-3">Telegram Login</Link>*/}
               </li>
             </ul>
 
@@ -64,55 +64,7 @@ function Header() {
 
           {/* Mobile menu */}
           <div className="md:hidden">
-
-            {/* Hamburger button */}
-            <button ref={trigger} className={`hamburger ${mobileNavOpen && 'active'}`} aria-controls="mobile-nav" aria-expanded={mobileNavOpen} onClick={() => setMobileNavOpen(!mobileNavOpen)}>
-              <span className="sr-only">Menu</span>
-              <svg className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <rect y="4" width="24" height="2" rx="1" />
-                <rect y="11" width="24" height="2" rx="1" />
-                <rect y="18" width="24" height="2" rx="1" />
-              </svg>
-            </button>
-
-            {/*Mobile navigation */}
-            <nav id="mobile-nav" ref={mobileNav} className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" style={mobileNavOpen ? { maxHeight: mobileNav.current.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: .8 } }>
-              <ul className="bg-gray-800 px-4 py-2">
-                <li>
-                  <Link to="/features" className="flex text-gray-300 hover:text-gray-200 py-2">Features</Link>
-                </li>
-                <li>
-                  <Link to="/pricing" className="flex text-gray-300 hover:text-gray-200 py-2">Pricing</Link>
-                </li>
-                <li>
-                  <Link to="/blog" className="flex text-gray-300 hover:text-gray-200 py-2">Blog</Link>
-                </li>
-                <li>
-                  <Link to="/about" className="flex text-gray-300 hover:text-gray-200 py-2">About us</Link>
-                </li>
-                <li className="py-2 my-2 border-t border-b border-gray-700">
-                  <span className="flex text-gray-300 py-2">Support</span>
-                  <ul className="pl-4">
-                    <li>
-                      <Link to="/contact" className="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">Contact us</Link>
-                    </li>
-                    <li>
-                      <Link to="/help" className="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">Help center</Link>
-                    </li>
-                    <li>
-                      <Link to="/404" className="text-sm flex font-medium text-gray-400 hover:text-gray-200 py-2">404</Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">Sign in</Link>
-                </li>
-                <li>
-                  <Link to="/signup" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Sign up</Link>
-                </li>
-              </ul>
-            </nav>
-
+            <MainEvent />
           </div>
 
         </div>
