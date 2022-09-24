939dcde068a3336edf4562f7988b8a3e8f6d465c
Neo87z <shehanhoradagoda12@gmail.com>
2022-09-24 18:53:30 -0230

    'origin;

--- src/partials/MobileView.jsx
+++ src/partials/MobileView.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState, useRef } from 'react';
 
 import FeatIllustration from '../images/chatBlack.jpeg';
 import FeatAvatar from '../images/features-avatar.jpg';
-import Tabs from './Tabs';
+import Tabs from './MobileTab';
 import LoveCHat from '../pages/LiveChat';
-import MainEvent from './Match';
+import MainEvent from './mobileMatch';
 import Axios from "axios";
 
 
@@ -22,7 +22,7 @@ function FeaturesHome() {
 
 
   return (
-    <section   style={{position:'absolute',marginLeft:'-30%'}}>
+    <section   style={{position:'absolute',marginLeft:'-0%',width:'100%'}}>
     
   <Tabs style={{position:'absolute'}} />
   <MainEvent  style={{position:'absolute'}}/>
